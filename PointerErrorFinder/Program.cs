7a585ff0279af40002577679c1cfe4cc3ca16da7
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-05-29 17:34:13 -0500

    Initial Commit.

---
+++ PointerErrorFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointerErrorFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
